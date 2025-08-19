5931b964823047dedf55edc01727cd94ab4697ba
patel_ankitb <patelankit25897@gmail.com>
2025-08-19 15:57:37 +0530

    commect the data.schema

--- src/data/data.schema.ts
+++ src/data/data.schema.ts
@@ -1,15 +1,15 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Collection, Document } from 'mongoose';
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { Collection, Document } from 'mongoose';
 
-export type IoTDataDocument = IoTData & Document;
+// export type IoTDataDocument = IoTData & Document;
 
-@Schema({ collection: 'iotdata' })
-export class IoTData {
-  @Prop({ type: Object })
-  data: Record<string, any>;
+// @Schema({ collection: 'iotdata' })
+// export class IoTData {
+//   @Prop({ type: Object })
+//   data: Record<string, any>;
 
-  @Prop({ type: Date })
-  time: Date;
-}
+//   @Prop({ type: Date })
+//   time: Date;
+// }
 
-export const IoTDataSchema = SchemaFactory.createForClass(IoTData);
+// export const IoTDataSchema = SchemaFactory.createForClass(IoTData);
